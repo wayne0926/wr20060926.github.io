1e76682fdeb41b23a593bb1bb06501fe5a3bee7b
wr20060926 <3462757126@qq.com>
2020-05-10 16:36:15 +0800

    格式化代码

--- tx/phone/js/jquery.beattext.js
+++ tx/phone/js/jquery.beattext.js
@@ -4,36 +4,37 @@
  * @Date	 2017-11-14 
  */
 
-(function($) {
-    $.fn.beatText = function(options) {
-        var defaults = {
-            beatHeight: '2em',
-            upTime: 700,
-            downTime: 700,
-			isAuth:true,
-			isRotate:true
-        };
-        var options = $.extend(defaults, options);
+(function ($) {
+	$.fn.beatText = function (options) {
+		var defaults = {
+			beatHeight: '2em',
+			upTime: 700,
+			downTime: 700,
+			isAuth: true,
+			isRotate: true
+		};
+		var options = $.extend(defaults, options);
 		console.log(options);
-        return this.each(function() {
-            var obj = $(this);
-            if (obj.text() !== obj.html()) {
-                return
-            };
-            var text = obj.text();
-            var newMarkup = '';
-            for (var i = 0; i <= text.length; i++) {
-                var character = text.slice(i, i + 1);
-                newMarkup += ($.trim(character)) ? '<span class="beat-char">' + character + '</span>' : character
-            }
-            obj.html(newMarkup);
-			if(!options.isAuth){			
-				obj.find('span.beat-char').each(function(index,el) {					
-					$(this).mouseover(function() {
-						beatAnimate($(this),options);
-					})							
+		return this.each(function () {
+			var obj = $(this);
+			if (obj.text() !== obj.html()) {
+				return
+			}
+			;
+			var text = obj.text();
+			var newMarkup = '';
+			for (var i = 0; i <= text.length; i++) {
+				var character = text.slice(i, i + 1);
+				newMarkup += ($.trim(character)) ? '<span class="beat-char">' + character + '</span>' : character
+			}
+			obj.html(newMarkup);
+			if (!options.isAuth) {
+				obj.find('span.beat-char').each(function (index, el) {
+					$(this).mouseover(function () {
+						beatAnimate($(this), options);
+					})
 				})
-			}else{
+			} else {
 				//自动跳动的动画
 				obj.find('span.beat-char:first').animate({
 					bottom: options.beatHeight
@@ -41,25 +42,26 @@
 					queue: false,
 					duration: options.upTime,
 					easing: 'easeOutCubic',
-					complete: function() {
+					complete: function () {
 						$(this).animate({
 							bottom: 0
 						}, {
 							queue: false,
 							duration: options.downTime,
 							easing: 'easeOutBounce',
-							complete:function(){
-								beatAnimate($(this).next(),options);
+							complete: function () {
+								beatAnimate($(this).next(), options);
 							}
 						})
 					}
 				});
 			}
-   
-        })
-    }
-	function beatAnimate(el,options){
-		if(options.isRotate){
+
+		})
+	}
+
+	function beatAnimate(el, options) {
+		if (options.isRotate) {
 			el.addClass("rotate");
 		}
 		el.animate({
@@ -68,7 +70,7 @@
 			queue: false,
 			duration: options.upTime,
 			easing: 'easeOutCubic',
-			complete: function() {
+			complete: function () {
 				el.removeClass("rotate");
 				$(this).animate({
 					bottom: 0
@@ -76,22 +78,22 @@
 					queue: false,
 					duration: options.downTime,
 					easing: 'easeOutBounce',
-					complete:function(){
-						if(options.isAuth){
+					complete: function () {
+						if (options.isAuth) {
 							var len = el.parent().children().length;
 							var indexNum = el.index();
-							if(indexNum == (len-1)){
-								beatAnimate(el.parent().find('span.beat-char:first'),options);
-							}else{
-								beatAnimate(el.next(),options);
+							if (indexNum == (len - 1)) {
+								beatAnimate(el.parent().find('span.beat-char:first'), options);
+							} else {
+								beatAnimate(el.next(), options);
 							}
 						}
 					}
 				})
 			}
 		})
-		
-		
+
+
 	}
 
 })(jQuery);
